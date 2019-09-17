6c10ef125efabc8fb19a2b31d8d1f23ba98c1ca0
jmccasusi <jmccasusi@gmail.com>
2019-09-17 16:56:25 -0400

    config action cable again

--- config/development.rb
+++ config/development.rb
@@ -1,4 +1,3 @@
 Rails.application.configure do 
-    config.action_cable.url = "ws://fanbase-gacha-api.herokuapp.com/cable"
-    config.action_cable.allowed_request_origins = [ /http:\/\/.*/, /https:\/\/.*/ ]
+    config.action_cable.url = "ws://localhost:3000/cable"
   end 
