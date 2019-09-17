a6aaca99bf5729a4b20be0fcb5c47e4841f8850b
jmccasusi <jmccasusi@gmail.com>
2019-09-17 16:39:52 -0400

    commit 4

--- config/development.rb
+++ config/development.rb
@@ -1,3 +1,4 @@
 Rails.application.configure do 
     config.action_cable.url = "ws://localhost:3000/cable"
+    Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:3001']
   end 
